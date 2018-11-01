d40d86c34c6ea6eb7c6a93b34e0e12a13f333ede
isuchang2 <43946293+isuchang2@users.noreply.github.com>
2018-10-31 23:45:20 -0700

    Add files via upload

--- test.py
+++ test.py
@@ -10,12 +10,11 @@ headers_post = {'Content-Type': 'application/json'}
 posturl = 'https://splunk.mocklab.io/movies'
 
 
-def setup_module(module):
-    print("setup_module      module:%s" % module.__name__)
-
-
 # Below are tests
 def test_no_two_movie_should_have_same_name():
     r = requests.get(geturl, headers=headers_get)
     t = json_normalize(json.loads(r.content), record_path='results')
     assert len(t.get('title')) == len(set(t.get('title')))
+
+def test_sample():
+    assert 3+5 == 8
